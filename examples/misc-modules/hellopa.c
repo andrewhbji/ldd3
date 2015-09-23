02ad4920f604aac2297262b5f71a31cc1754ad48
andrewpetrovic <andrewpetrovic@gmail.com>
2015-09-23 12:09:34 +0800

    将数组模块参数内容从hellop.c迁移至hellopa.c

--- examples/misc-modules/hellopa.c
+++ examples/misc-modules/hellopa.c
@@ -1,11 +1,12 @@
 /*                                                     
- * $Id: hellop.c,v 1.4 2004/09/26 07:02:43 gregkh Exp $ 
+ * $Id: hellopa.c,v 1.0 2015/09/23 07:02:43 gregkh Exp $ 
  */                                                    
 #include <linux/init.h>
 #include <linux/module.h>
 #include <linux/moduleparam.h>
 
 MODULE_LICENSE("Dual BSD/GPL");
+#define ARRAYSIZE 10
 /*                                                        
  * These lines, although not shown in the book,           
  * are needed to make hello.c run properly even when      
@@ -14,21 +15,23 @@ MODULE_LICENSE("Dual BSD/GPL");
                                                           
 
 /*
- * 测试模块参数
- * char 指针 whom 接收输入的字符串,int 变量howmany接收输入的数值,如
- * $ insmod hellop.ko howmany=10 whom="Mom"
+ * 测试数组模块参数
+ * 装载模块时指定数组参数,参数内容用逗号隔开,如:
+ * $ insmod hellopa.ko array=1,2,3,4,5
  */
-static char *whom = "world";
-static int howmany = 1;
+static int array[ARRAYSIZE];
+int narr;
 
-module_param(howmany, int, S_IRUGO);
-module_param(whom, charp, S_IRUGO);
+/*
+ * 将narr的地址传递给module_param_array函数,接收数组长度
+ */
+module_param_array(array,int,&narr,S_IRUGO);
 
 static int hello_init(void)
 {
 	int i;
-	for (i = 0; i < howmany; i++)
-		printk(KERN_ALERT "(%d) Hello, %s\n", i, whom);
+	for (i = 0; i < narr; i++)
+		printk(KERN_ALERT "array[%d] = %d\n", i, array[i]);
 	return 0;
 }
 
